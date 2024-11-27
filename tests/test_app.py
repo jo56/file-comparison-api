199cc55b6810b0dd97d3c086e4d8827fc6b3d697
John O'Farrell <johnofarrell12@gmail.com>
2024-11-27 13:17:31 -0500

    Updating filenames

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,8 +22,8 @@ class AppTests(unittest.IsolatedAsyncioTestCase):
     async def test_md_compare(self):
 
         current_dir = os.path.dirname(__file__)
-        file1_path =  os.path.join(current_dir, "test_files",  "text", "text_file1.md")
-        file2_path =  os.path.join(current_dir, "test_files", "text", "text_file2.md")
+        file1_path =  os.path.join(current_dir, "test_files",  "markdown", "md_file1.md")
+        file2_path =  os.path.join(current_dir, "test_files", "markdown", "md_file2.md")
 
         upload_file1 = create_upload_file_from_local(file1_path)
         upload_file2 = create_upload_file_from_local(file2_path)
